d4b5a762153a36c40cf9b3854d7a23840cceac95
Michael Zappa <michael.zappa@gmail.com>
2024-04-15 21:08:12 -0600

    linter ...
    Signed-off-by: Michael Zappa <michael.zappa@gmail.com>

--- errors.go
+++ errors.go
@@ -21,12 +21,12 @@ import (
 )
 
 var (
-	ErrCNINotInitialized    = errors.New("cni plugin not initialized")
-	ErrInvalidConfig        = errors.New("invalid cni config")
-	ErrNotFound             = errors.New("not found")
-	ErrRead                 = errors.New("failed to read config file")
-	ErrInvalidResult        = errors.New("invalid result")
-	ErrLoad                 = errors.New("failed to load cni config")
+	ErrCNINotInitialized = errors.New("cni plugin not initialized")
+	ErrInvalidConfig     = errors.New("invalid cni config")
+	ErrNotFound          = errors.New("not found")
+	ErrRead              = errors.New("failed to read config file")
+	ErrInvalidResult     = errors.New("invalid result")
+	ErrLoad              = errors.New("failed to load cni config")
 )
 
 // IsCNINotInitialized returns true if the error is due to cni config not being initialized
